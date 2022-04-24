baa757475a308a813523caacdfe3c8d6dbb5232d
Md-Sefat-Alam <md.sefatalam@gmail.com>
2022-04-24 15:39:45 +0600

    recently added product in home

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -11,9 +11,21 @@ import HomeProduct3Show from "../../shared/HomeProduct3Show/HomeProduct3Show";
 
 // https://salam-furniture-mart.herokuapp.com/product/3/bed
 const Home = () => {
+  const [renectAdded, setRecentAdded] = useState({});
   const [bedData, setBedData] = useState({});
   const [sofaData, setSofaData] = useState({});
   const [officeData, setOfficeData] = useState({});
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/recent-product-added")
+      .then((res) => {
+        if (res.status === 200) {
+          setRecentAdded(res.data);
+        }
+      })
+      .catch((error) => {})
+      .finally(() => {});
+  }, []);
   useEffect(() => {
     axios
       .get("https://salam-furniture-mart.herokuapp.com/product/3/bed")
@@ -51,39 +63,15 @@ const Home = () => {
     <div>
       <TopRatingProductBanner />
       <Categories />
-      <div style={{ backgroundColor: "#FEFBF3" }} className="py-24">
-        <ContentHeader
-          hText={"New collections"}
-          dText="some new collection may you choose it.."
-        />
-        <Container maxWidth="xl">
-          <div className="">
-            <Masonry
-              columns={{ xs: 1, sm: 1, md: 2, lg: 3, xl: 3 }}
-              spacing={2}
-            >
-              {[1, 2, 3].map((single) => (
-                <ProductShowCart
-                  key={single}
-                  imgLink={
-                    "https://cdn.pixabay.com/photo/2021/08/27/01/33/bedroom-6577523_960_720.jpg"
-                  }
-                  ratingValue={3.5}
-                  productId="Bed100001"
-                />
-              ))}
-            </Masonry>
-            <div className="text-right">
-              <Button
-                style={{ backgroundColor: "#212121", color: "white" }}
-                variant="outlined"
-              >
-                See More...
-              </Button>
-            </div>
-          </div>
-        </Container>
-      </div>
+      {renectAdded.count > 0 && (
+        <div style={{ backgroundColor: "#FEFBF3" }} className="py-24">
+          <ContentHeader
+            hText={"recently added new products"}
+            dText="some recently added new products collection may you choose it.."
+          />
+          <HomeProduct3Show data={renectAdded} />
+        </div>
+      )}
       {sofaData.count > 0 && (
         <div style={{ backgroundColor: "#EEEEEE" }} className="py-24">
           <ContentHeader
